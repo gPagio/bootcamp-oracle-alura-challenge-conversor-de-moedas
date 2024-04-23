3ebdefe6bcbdd65ea54e976298681b9c5f2ac7bd
Guilherme Pagio <116508581+gPagio@users.noreply.github.com>
2024-04-23 17:33:22 -0300

    fix: validar numeros big decimais
    Corrige a validacao que era realizado em double e passa a ser BigDecimal

--- src/main/java/br/com/alura/oracle/one/guilhermepagio/util/input/InputValidator.java
+++ src/main/java/br/com/alura/oracle/one/guilhermepagio/util/input/InputValidator.java
@@ -1,12 +1,23 @@
 package br.com.alura.oracle.one.guilhermepagio.util.input;
 
+import java.math.BigDecimal;
+
 public class InputValidator {
 
     private InputValidator (){}
 
-    public static boolean ContemApenasNumerosInteiros(String entrada) {
+    public static boolean contemApenasNumerosInteiros(String entrada) {
         try {
-            Integer.parseInt(entrada);
+            Integer i = Integer.parseInt(entrada);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean contemApenasNumerosBigDecimais(String entrada) {
+        try {
+            BigDecimal b = new BigDecimal (entrada);
             return true;
         } catch (Exception e) {
             return false;
