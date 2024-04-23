2cb4cfa31bb25cf067e96fced30d3d3790ebef38
gPagio <guilhermepagio.contato@gmail.com>
2024-04-22 21:12:46 -0300

    refactor: delega funcoes para helpers
    Delega funcoes complexas para metodos especificos

--- src/main/java/br/com/alura/oracle/one/guilhermepagio/Main.java
+++ src/main/java/br/com/alura/oracle/one/guilhermepagio/Main.java
@@ -1,9 +1,12 @@
 package br.com.alura.oracle.one.guilhermepagio;
 
 import br.com.alura.oracle.one.guilhermepagio.calculo.ConversorMoeda;
+import br.com.alura.oracle.one.guilhermepagio.helper.DecodificadorMoeda;
 import br.com.alura.oracle.one.guilhermepagio.view.Menus;
 
 import java.math.BigDecimal;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,64 +17,10 @@ public class Main {
             String moedaConvertida = "";
 
             opcao = Menus.obterOpcoesMenuPrincipal();
-
-            switch (opcao){
-                case 0:
-                    break;
-                case 1:
-                    moedaBase = "BRL";
-                    moedaConvertida = "USD";
-                    break;
-                case 2:
-                    moedaBase = "BRL";
-                    moedaConvertida = "EUR";
-                    break;
-                case 3:
-                    moedaBase = "BRL";
-                    moedaConvertida = "JPY";
-                    break;
-                case 4:
-                    moedaBase = "USD";
-                    moedaConvertida = "BRL";
-                    break;
-                case 5:
-                    moedaBase = "USD";
-                    moedaConvertida = "EUR";
-                    break;
-                case 6:
-                    moedaBase = "USD";
-                    moedaConvertida = "JPY";
-                    break;
-                case 7:
-                    moedaBase = "EUR";
-                    moedaConvertida = "BRL";
-                    break;
-                case 8:
-                    moedaBase = "EUR";
-                    moedaConvertida = "USD";
-                    break;
-                case 9:
-                    moedaBase = "EUR";
-                    moedaConvertida = "JPY";
-                    break;
-                case 10:
-                    moedaBase = "JPY";
-                    moedaConvertida = "BRL";
-                    break;
-                case 11:
-                    moedaBase = "JPY";
-                    moedaConvertida = "USD";
-                    break;
-                case 12:
-                    moedaBase = "JPY";
-                    moedaConvertida = "EUR";
-                    break;
-                default:
-                    break;
-            }
+            String [] moedas = DecodificadorMoeda.decodifica(opcao);
 
             try {
-                BigDecimal moeda = ConversorMoeda.converteMoeda(moedaBase, 100, moedaConvertida);
+                BigDecimal moeda = ConversorMoeda.converteMoeda(moedas[0], 100, moedas[1]);
                 System.out.println(moeda);
             } catch (Exception e){
                 System.out.println("Deu ruim");
