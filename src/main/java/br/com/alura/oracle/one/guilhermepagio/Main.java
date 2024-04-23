fcc504c3b2c671f2d1d5c554f8dcc431532f7f42
Guilherme Pagio <116508581+gPagio@users.noreply.github.com>
2024-04-23 17:39:21 -0300

    refactor: mensagem com os valores convertidos
    Aprimora a mensagem com o retorno contendo os valores convertidos
    
    Melhorias no fluxo de execução da aplicação

--- src/main/java/br/com/alura/oracle/one/guilhermepagio/Main.java
+++ src/main/java/br/com/alura/oracle/one/guilhermepagio/Main.java
@@ -1,31 +1,32 @@
 package br.com.alura.oracle.one.guilhermepagio;
 
 import br.com.alura.oracle.one.guilhermepagio.calculo.ConversorMoeda;
-import br.com.alura.oracle.one.guilhermepagio.helper.DecodificadorMoeda;
+import br.com.alura.oracle.one.guilhermepagio.helper.Decodificador;
+import br.com.alura.oracle.one.guilhermepagio.util.output.OutputFormater;
 import br.com.alura.oracle.one.guilhermepagio.view.Menus;
 
 import java.math.BigDecimal;
-import java.util.Map;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int opcao = Integer.MIN_VALUE;
+        int opcao;
 
-        while (opcao != 0){
-            String moedaBase = "";
-            String moedaConvertida = "";
-
+        do {
             opcao = Menus.obterOpcoesMenuPrincipal();
-            String [] moedas = DecodificadorMoeda.decodifica(opcao);
+            String [] moedas = Decodificador.decodificaMoedasDasOpcaoesDoMenu(opcao);
+
+            if (opcao == 0) {
+                break;
+            }
+            BigDecimal quantidadeMoedaBase = Menus.obterQuantidadeMoedaBase();
 
             try {
-                BigDecimal moeda = ConversorMoeda.converteMoeda(moedas[0], 100, moedas[1]);
-                System.out.println(moeda);
+                BigDecimal moedaConvertida = ConversorMoeda.converteMoeda(moedas[0], quantidadeMoedaBase, moedas[1]);
+                System.out.println(moedas[0] + " " + OutputFormater.numeroDuasCasasDecimais(quantidadeMoedaBase) + " é equivalente a " + moedas[1] + " " + OutputFormater.numeroDuasCasasDecimais(moedaConvertida));
             } catch (Exception e){
                 System.out.println("Deu ruim");
                 System.out.println(e.getMessage());
             }
-        }
+        } while (opcao != 0);
     }
 }
