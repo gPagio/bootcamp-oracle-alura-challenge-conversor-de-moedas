82730a17c4883ede107ccf31df02c38cecae73d8
gPagio <guilhermepagio.contato@gmail.com>
2024-04-22 21:09:26 -0300

    feat: first commit
    Decodificador de moeda baseado nas opcoes do menu

---
+++ src/main/java/br/com/alura/oracle/one/guilhermepagio/helper/DecodificadorMoeda.java
@@ -0,0 +1,73 @@
+package br.com.alura.oracle.one.guilhermepagio.helper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DecodificadorMoeda {
+
+    private DecodificadorMoeda (){}
+
+    public static String [] decodifica (int opcao){
+        String [] keyValue = new String[2];
+
+        switch (opcao){
+            case 0:
+                System.out.println("Obrigado por usar o conversor de moedas!");
+                break;
+            case 1:
+                keyValue[0] = "BRL";
+                keyValue[1] = "USD";
+                break;
+            case 2:
+                keyValue[0] = "BRL";
+                keyValue[1] = "EUR";
+                break;
+            case 3:
+                keyValue[0] = "BRL";
+                keyValue[1] = "JPY";
+                break;
+            case 4:
+                keyValue[0] = "USD";
+                keyValue[1] = "BRL";
+                break;
+            case 5:
+                keyValue[0] = "USD";
+                keyValue[1] = "EUR";
+                break;
+            case 6:
+                keyValue[0] = "USD";
+                keyValue[1] = "JPY";
+                break;
+            case 7:
+                keyValue[0] = "EUR";
+                keyValue[1] = "BRL";
+                break;
+            case 8:
+                keyValue[0] = "EUR";
+                keyValue[1] = "USD";
+                break;
+            case 9:
+                keyValue[0] = "EUR";
+                keyValue[1] = "JPY";
+                break;
+            case 10:
+                keyValue[0] = "JPY";
+                keyValue[1] = "BRL";
+                break;
+            case 11:
+                keyValue[0] = "JPY";
+                keyValue[1] = "USD";
+                break;
+            case 12:
+                keyValue[0] = "JPY";
+                keyValue[1] = "EUR";
+                break;
+            default:
+                System.out.println("Opção inválida!");
+                System.out.println("Digite apenas opções do menu abaixo!");
+                break;
+        }
+        return keyValue;
+    }
+
+}
