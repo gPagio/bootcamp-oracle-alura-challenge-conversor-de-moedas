897a7249529a904c4a19ed976fca7a9732cd96ea
gPagio <guilhermepagio.contato@gmail.com>
2024-04-24 02:02:40 -0300

    fix(textos): erros de ortografia
    Corrige erros de ortografia seguinto o realizado em [f2d2a56ec355ea7c3aa399dd3dbb42a2db861131]

--- src/main/java/br/com/alura/oracle/one/guilhermepagio/view/Menus.java
+++ src/main/java/br/com/alura/oracle/one/guilhermepagio/view/Menus.java
@@ -10,17 +10,17 @@ public class Menus {
 
     public static int obterOpcoesMenuPrincipal () {
         System.out.println("0 - Sair");
-        System.out.println("1 - Converter Real para Dolar Americano");
+        System.out.println("1 - Converter Real para Dólar Americano");
         System.out.println("2 - Converter Real para Euro");
         System.out.println("3 - Converter Real para Iene Japonês");
-        System.out.println("4 - Converter Dolar Americano para Real");
-        System.out.println("5 - Converter Dolar Americano para Euro");
-        System.out.println("6 - Converter Dolar Americano para Iene Japonês");
+        System.out.println("4 - Converter Dólar Americano para Real");
+        System.out.println("5 - Converter Dólar Americano para Euro");
+        System.out.println("6 - Converter Dólar Americano para Iene Japonês");
         System.out.println("7 - Converter Euro para Real");
-        System.out.println("8 - Converter Euro para Dolar Americano");
+        System.out.println("8 - Converter Euro para Dólar Americano");
         System.out.println("9 - Converter Euro para Iene Japonês");
         System.out.println("10 - Converter Iene Japonês para Real");
-        System.out.println("11 - Converter Iene Japonês para Dolar Americano");
+        System.out.println("11 - Converter Iene Japonês para Dólar Americano");
         System.out.println("12 - Converter Iene Japonês para Euro");
         return Teclado.capturaInteiroDigitado();
 
